440386af34debdd5adf37629f7f768fdb1f93164
Drew Robinson <drew.robinson@gmail.com>
2016-08-09 16:41:42 +0800

    Made screenshot larger, tweaked file name.

--- tests/phantomjs/front_page.js
+++ tests/phantomjs/front_page.js
@@ -2,6 +2,8 @@
 
 /* global require phantom */
 var page = require('webpage').create();
+page.viewPortSize = {width: 1280, height: 1024};
+page.clipRect = {top: 0, left: 0, width: 1280, height: 1024};
 page.open('http://127.0.0.1:8080/', function () {
   'use strict';
   page.render('front_page.png');
