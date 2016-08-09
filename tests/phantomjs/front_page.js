08f3fdaa288a49842b5dd992a84dd36df6cdc4ba
Drew Robinson <drew.robinson@gmail.com>
2016-08-09 11:58:29 +0800

    Moved screenshot date stamping out to Travis, removed file.io SSL test.

--- tests/phantomjs/front_page.js
+++ tests/phantomjs/front_page.js
@@ -4,14 +4,6 @@
 var page = require('webpage').create();
 page.open('http://127.0.0.1:8080/', function () {
   'use strict';
-  var now = new Date();
-  var Y = now.getFullYear();
-  var M = now.getMonth();
-  var d = now.getDate();
-  var h = now.getHours();
-  var m = now.getMinutes();
-  var s = now.getSeconds();
-  var fileName = 'front_page-' + Y + M + d + '-' + h + m + s + '.png';
-  page.render(fileName);
+  page.render('front_page.png');
   phantom.exit();
 });
