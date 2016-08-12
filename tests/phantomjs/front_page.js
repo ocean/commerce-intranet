b25b735aae5ddc9d1de8020912e6d51dc7701737
Drew Robinson <drew.robinson@gmail.com>
2016-08-12 10:23:42 +0800

    Added reload command to screenshot test to make sure page loads.

--- tests/phantomjs/front_page.js
+++ tests/phantomjs/front_page.js
@@ -4,10 +4,8 @@
 /* eslint-disable no-console */
 
 var page = require('webpage').create();
-page.settings = {
-  userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/601.6.17 (KHTML, like Gecko) Version/9.1.1 Safari/601.6.17',
-  resourceTimeout: 4500
-};
+page.settings.userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/601.6.17 (KHTML, like Gecko) Version/9.1.1 Safari/601.6.17';
+
 page.viewPortSize = {
   width: 1280,
   height: 1024
@@ -22,6 +20,7 @@ page.onConsoleMessage = function (msg, lineNum, sourceId) {
 page.open('http://127.0.0.1:8080/', function (status) {
   'use strict';
   console.log('PhantomJS status: ' + status);
+  page.reload();
   page.render('front_page.png');
   phantom.exit();
 });
