ce4a5932bf28da40601255f152dc84d1c0f8b6ac
Drew Robinson <drew.robinson@gmail.com>
2016-08-25 14:10:06 +0800

    First version of deployment automation scripts for dev environment: - Git post-receive hook runs on push, calls... - Simple deploy.sh script in repo directory, which calls... - Ansible playbook which does the actual deployment work on the server.

---
+++ scripts/deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+################################################################################
+#
+# Generic command runner script
+# 
+# Run deployment scripts / Ansible playbooks as required.
+#
+# Fired from git post-receive hook within server repository.
+#
+################################################################################
+
+if [ $# -lt 3 ]
+then
+    echo "usage - $0 [old git revision] [new git revisions] [git ref name]"
+    exit 1
+fi
+
+oldrev="$1"
+newrev="$2"
+refname="$3"
+ansible_cmd="$(which ansible-playbook)"
+
+if [ -x "$ansible_cmd" ]; then
+  $ansible_cmd ansible/deploy/dev-playbook.yml --extra-vars "git_old_revision=$oldrev git_new_revision=$newrev git_refname=$refname"
+fi
