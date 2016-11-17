d963fc23e13bf43032a430e3f713c0df2f1239d7
Drew Robinson <drew.robinson@gmail.com>
2016-11-17 17:55:22 +0800

    ✨ Added new super simple block generated by the News Centre module.

---
+++ web/modules/custom/news_centre/src/Plugin/Block/NewsBlock.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Drupal\news_centre\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'NewsBlock' block.
+ *
+ * @Block(
+ *  id = "news_block",
+ *  admin_label = @Translation("News"),
+ * )
+ */
+class NewsBlock extends BlockBase {
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build = [];
+    $build['news_block']['#markup'] = 'This is the news block, which will be configurable to hold stuff.';
+
+    return $build;
+  }
+
+}
