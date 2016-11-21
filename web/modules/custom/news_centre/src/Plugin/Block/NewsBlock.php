b6f281880d349fbd068f3ea75428c13fb2530eca
Drew Robinson <drew.robinson@gmail.com>
2016-11-21 17:56:43 +0800

    News module:
    A block themed with Twig, yay!

--- web/modules/custom/news_centre/src/Plugin/Block/NewsBlock.php
+++ web/modules/custom/news_centre/src/Plugin/Block/NewsBlock.php
@@ -19,10 +19,17 @@ class NewsBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function build() {
-    $build = [];
-    $build['news_block']['#markup'] = 'This is the news block, which will be configurable to hold stuff.';
+    // $build = [];
+    // $build['news_block']['#markup'] = 'This is the news block, which will be configurable to hold stuff.';
 
-    return $build;
+    // return $build;
+
+    $contents = 'This is a news block content field, used to hold stuff.';
+
+    return [
+      '#theme' => 'news_centre_block',
+      '#data' => $contents
+    ];
   }
 
 }
